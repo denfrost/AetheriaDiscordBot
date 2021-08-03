eedd0daffdc77f603d207fade4362dd433b64040
AgentLoneStar007 <57960152+AgentLoneStar007@users.noreply.github.com>
2021-08-03 12:24:31 +0000

    More work on events function.

--- cogs/eventcog.py
+++ cogs/eventcog.py
@@ -2,17 +2,23 @@ import discord # Currently unused. Used in sending Discord messages.
 from discord.ext import commands
 import json
 
+def split(word):
+      return [char for char in word]
+
 class Events(commands.Cog): # Defines the cog name.
     def __init__(self, bot): # Initializes the cog.
         self.bot = bot
 
     @commands.command(name='addevent', help='Add an event to the event list. Usage: `addevent <name of event> <time to execute, format dd:hh:mm>')
     async def addevent(self, ctx, name: str, time: str): # Adds a simple template command.
-        i = 0
-        events = open("events.json","w+")
-        await events.write(time + "\n")
-        await events.write(name)
-        await ctx.send('Added event "' + name + '" to event register.')
+        with open('events.json','r+') as file:
+          data = json.loads(file.read())
+        time1 = split(time)
+        day = time1[0] + time1[1]
+        hour = time1[3] + time1[4]
+        minute = time1[6] + time1[7]
+        entry = '{"name": '+ name +', "time": {"day": ' + day + ',"hour": ' + hour + ',"minute": '+ minute +',"executed": false}'
+        data['events'].append(entry)
 
 def setup(bot): # Loads the cog into the bot.
     bot.add_cog(Events(bot))
--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import json
 
+def split(word):
+    return [char for char in word]
+
 with open('cogs/events.json','r') as file:
   data = json.loads(file.read())
 
 while True:
-  io = input ('Current Time: ')
+  inp = input ('Current Time: ')
+  temp = split(inp)
+  day = temp[0] + temp[1]
+  hour = temp[3] + temp[4]
+  minute = temp[6] + temp[7]
   for event in data['events']:
-    if event['time'] == io:
+    if event['time'] == inp:
       name = event['name']
       print(f'Executing event {name}.')
-    elif event['time'] != io:
+    elif event['time'] != inp:
       name = event['name']
       print(f'Event {name}\'s provided time does not match current time.')
