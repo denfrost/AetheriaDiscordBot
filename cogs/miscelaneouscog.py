9842c6345636acb4e3b5e926c2abc6e38cbf14c5
AgentLoneStar <>
2021-08-20 14:24:56 +0000

    Added version command. Made chaning bot version easier.

--- cogs/miscelaneouscog.py
+++ cogs/miscelaneouscog.py
@@ -2,17 +2,23 @@ import discord # Currently unused. Used in sending Discord messages.
 from discord.ext import commands
 from random import randint
 
-class Miscellaneous(commands.Cog, description='Random, unassigned commands.'): # Defines the cog name.
-    def __init__(self, bot): # Initializes the cog.
+class Miscellaneous(commands.Cog, description='Random, unassigned commands.'):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='buzzword', help='Prints a random buzzword.')
-    async def command(self, ctx): # Adds a simple template command.
+    async def buzzword(self, ctx):
         buzzwordList = ["Advertainment", "Big Data", "Content Is King", "Customer Journey", "Deep Dive", "Growth Hacking", "Hyperlocal", "Low Hanging Fruit", "Move the Needle", "Retargeting", "Alignment", "Disruptor/Disruptive", "Freemium", "Leverage", "Quick Win", "Quota", "Value Add", "Wheelhouse", "Customer Acquisition", "Customer-Centric/Customer Centricity", "Customer Lifecycle", "Customer Retention", "Personalization", "Touchpoint", "Voice of the Customer", "Clickbait", "Earned Media", "Live Streaming", "Micro-Influencer", "User-Generated Content", "FOMO", "Lit", "Spilling Tea", "TL;DR"]
         randChoice = randint(0, len(buzzwordList)-1)
         buzzword = buzzwordList[randChoice]
         await ctx.send(buzzword)
+    
+    @commands.command(name='version', help='Shows the current version of the bot.')
+    async def version(self, ctx):
+      with open('./static/version.txt', "r") as file:
+        version = file.read()
+      await ctx.send('Current bot version: `' + version + '`')
 
-def setup(bot): # Loads the cog into the bot.
+def setup(bot):
     bot.add_cog(Miscellaneous(bot))
     print('Miscellaneous cog successfully loaded.')
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,12 @@
+const response = await fetch('https://AetheriaDiscordBot.agentlonestar.repl.co/static/version.txt');
+const version = await response.text();
+
 $('body').terminal({
   test: function() {
     this.echo('Test')
     }
 }, {
-  greetings: 'Pointy-Haired Bot Control Console\nVersion 0.8\nCopyright AgentLoneStar007, All Rights Reserved\n',
+  greetings: 'Pointy-Haired Bot Control Console\nVersion ' + version + '\nCopyright AgentLoneStar007, All Rights Reserved\n',
   prompt: '>>> '
 });
 
