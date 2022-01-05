e8effe9a696282ee39f35240bd390a4bdfeccb18
AgentLoneStar007 <57960152+AgentLoneStar007@users.noreply.github.com>
2022-01-05 13:18:15 +0000

    Temp

--- cogs/eventcog.py
+++ cogs/eventcog.py
@@ -11,17 +11,16 @@ class Events(commands.Cog, description='Create and manage events.'):
 
     @commands.command(name='addevent', help='Add an event to the event list. Usage: `addevent <name of event> <time to execute, format dd:hh:mm>')
     async def addevent(self, ctx, name: str, time: str):
-      await ctx.send('This feature is not complete yet.')
-        #with open('events.json', "r") as file:
-        #  data = json.load(file)
-        #time1 = split(time)
-        #day = time1[0] + time1[1]
-        #hour = time1[3] + time1[4]
-        #minute = time1[6] + time1[7]
-        #entry = '{"name": '+ name +', "time": {"day": ' + day + ',"hour": ' + hour + ',"minute": '+ minute +',"executed": false}'
-        #data['events'].append(entry)
-        #with open('events.json', "w") as file:
-        #  json.dump(data, file)
+      with open('events.json', "r") as file:
+        data = json.load(file)
+      time1 = split(time) 
+      day = time1[0] + time1[1]
+      hour = time1[3] + time1[4]
+      minute = time1[6] + time1[7]
+      entry = '{"name": '+ name +', "time": {"day": ' + day + ',"hour": ' + hour + ',"minute": '+ minute +',"executed": false}'
+      data['events'].append(entry)
+      with open('events.json', "w") as file:
+        json.dump(data, file)
 
 def setup(bot):
     bot.add_cog(Events(bot))
--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ const response = await fetch('https://phbconsole.ddns.net/static/version.txt');
 const version = await response.text();
 
 $('body').terminal({
-  test: function() {
+  help: function() {
     this.echo('Test')
     }
 }, {
